7fbff508f4203603f9b9242f312568277e4bdebd
buildsyatharth <yatharthdew@gmail.com>
2026-03-13 23:10:22 +0530

    Add lecture notes

---
+++ Architect_Sprint/02_Python_Stringes_Function/Lecture_motes.py
@@ -0,0 +1,18 @@
+# escape squance character are special charatr for formating
+ 
+str = "this is a strimg. \nwe are creating in python"
+print(str)
+str0 = "this is a strimg. \twe are creating in python"
+print(str0)
+
+# Operation 
+
+# Concatention 
+str1 = "my world"
+str2 = "doremon"
+strF = str1 + str2
+print(strF)
+
+
+
+
