ca5e9dcb5a72ae8c6ef3f167afd932d80dfbcd27
buildsyatharth <yatharthdew@gmail.com>
2026-03-13 18:13:26 +0530

    Day 1: Initialized my AIML journey

---
+++ Architect_Sprint/01_Python_Variable_Datatype/Lecture_notes.py
@@ -0,0 +1,151 @@
+print("hello world")
+print = function 
+print("yatharth is my name")
+print("My age is 18")
+print("my name is yatharth", "my dream is to become AIML")
+print(" give me one year pls", " please")
+
+print(23)
+print(20/4)
+
+name = "yatharth" 
+
+## here name is a variable double quote is the defination of string 
+
+print(name)
+age = 23
+price = 25.55
+print(age)
+print("my name is : ", name)
+print("price :", price)
+age2 = age
+print("my age is : ", age)
+print("my name is :" , age)
+vivo = "Phone"
+print("my phone is :" , vivo)
+print("my age is :" , age2)
+name = "yathrth"
+age = 67
+price = 99.77
+age = 23
+old = False
+a = None
+
+print(type(age))
+print(type(old))
+print(type(a))
+
+## print(type(anything)) is a code to know which type of data it is stir int or float
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+# below is the basic problem of python addition 
+
+a = 55
+b = 77
+sum = a+b
+print(a + b)
+print(type(a))
+
+## arthmetic operators
+a = 5
+b = 2
+
+sum = a+b
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a % b) # remainder (modulo)
+print(a ** b) # a to the power b
+
+# # ! generally represent NOT 
+
+# realtional operators
+a = 50
+b = 20
+print(a == b) # false 
+print(a != b ) # true
+print(a >= b)
+print(a <= b)
+print(a > b)
+print(a < b)
+
+## assignment operators
+num = 10
+num = num + 10 # 10+10 =>20
+num += 10
+num1 = 10
+num1 -= 5
+num2 = 10
+num2 /= 2
+num3 = 10
+num3 *= 10
+num4 = 10
+num4 %= 3
+num5 = 10
+num5 **= 10
+
+print("num:", num)
+print("num:", num1)
+print("num:", num2)
+print("num:", num3)
+print("num:", num4)
+print("num:", num5)
+
+# # LOgical operators
+
+print(not False)
+print(not True)
+a= 50
+b=30
+print(not (a>b))
+
+val1 = True
+val2 = True
+print("ANd operator", val1 and val2 )
+
+print("OR operator", val1 or val2 )
+
+print(" OR operator:", (a ==b ) or (a >b))
+
+# type conversion
+
+a = 2
+b = 4.35
+
+sum = a+b  # 2.0  + 4.25 => 6.35
+
+print("sum1: ", sum)
+a1 = int("2") # type conversion 
+b1 = 4.35
+
+sum = a + b
+print ("sum:", sum)
+print(type(a))
+
+## Type casting
+
+a = 66.55
+a = str(a)
+print(a)
+print(type(a))
+
+val = input("enter your value")
+print(type(val), val) # "25" , "45.77"
+
+int("5")
+val =float(input("enter your value"))
+print(type(val), val) # "25" , "45.77"
+
+
+name = input("enter name:")
+age = int(input("enter age:"))
+markes = float(input ("enter marks:"))
+
+print("welcome", name)
+print("age =", age)
+print("makers =", markes)
+
