30101cf8050a05c8297452216452d0295243729b
buildsyatharth <yatharthdew@gmail.com>
2026-03-19 23:48:09 +0530

    Add Day 07  practice and nested logic work

---
+++ AIML-Architect-sprint/PRACTICE.py
@@ -0,0 +1,29 @@
+#PRACTICE QUESTION
+
+#1
+no = 10
+while no <= 15:
+    print(no, end=" ")
+    no += 1
+
+#2
+i = 1
+while i <= 5:
+    print(i ** 3, end=" ")
+    i += 1
+
+#3
+i2 = 1
+while i2 <= 10:
+    if i2 %2 != 0:
+        print(i2, end=" ")
+    i2 += 1
+
+#4
+p = 1
+n = 1
+while n <= 5:
+    p *= n
+    n += 1
+print(p)
+    
