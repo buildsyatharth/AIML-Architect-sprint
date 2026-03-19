fab4acf2ec547086bbf824c9a78d2e8bdc727211
buildsyatharth <yatharthdew@gmail.com>
2026-03-20 00:52:11 +0530

    Add Day 07 practice and nested logic work

--- AIML-Architect-sprint/PRACTICE.py
+++ AIML-Architect-sprint/PRACTICE.py
@@ -26,4 +26,80 @@ while n <= 5:
     p *= n
     n += 1
 print(p)
-    
+
+#5
+s = input("Enter your sentance : ")
+words = s.split()
+for word in words:
+    i = len(word) - 1
+    while i >= 0:
+        print(word[i], end= " ")
+        i -= 1
+    print(end= " ")
+
+#6
+word = input("Enter a word to check : ")
+vowels = "aeiou"
+count = 0
+index = 0
+
+while index < len(word):
+    if word[index].lower() not in vowels and word[index].isalpha():
+        count +=1
+    index += 1
+print(f"No. of consonents : ", count )
+
+#7
+multiple = 3
+count = 1
+
+while count <= 5:
+    print(multiple, end= " ")
+    multiple += 3
+    count += 1
+#OR
+a = 1
+b = 3
+while a <= 5:
+    mu = a*b
+    print(mu,)
+    a += 1
+
+#8
+i = 1
+a = int(input("Enter your no : "))
+b = int(input("Enter the power of exponent : "))
+result = a
+while i < b:
+    result = result * a
+    i += 1
+print("Answer :", result)
+
+#9
+num = int(input("Enter a no. to check = "))
+i1 = 1
+is_perfect_square = False
+
+while i1 * i1 <= num:
+    if i1 * i1 == num:
+        is_perfect_square = True
+        break
+    i1 += 1
+
+if is_perfect_square:
+    print(f"{num} is a perfect square")
+
+else:
+    print(f"{num} is not a perfect square")
+
+#10
+string = input("Enter a string : ")
+char_to_count = input("Enter a character to count : ")
+count = 0
+index = 0 
+
+while index < len(string):
+    if string[index] == char_to_count:
+        count += 1
+    index += 1
+print(f"{char_to_count} + {count}")
